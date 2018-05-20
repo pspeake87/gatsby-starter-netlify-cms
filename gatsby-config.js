cb9815adc423ee755ecadc17ada1f8d0112cf9f1
Philip Speake <philip_speake@yahoo.com>
2018-05-19 22:06:59 -0700

    added sitemap and robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,15 @@
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://www.speakesoftware.co',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 module.exports = {
   siteMetadata: {
+    siteUrl,
     title: 'Speake Software',
   },
   plugins: [
@@ -27,6 +37,39 @@ module.exports = {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: isNetlifyProduction
+        ? { policy: [{ userAgent: '*' }] }
+        : {
+          policy: [{ userAgent: '*', disallow: ['/'] }],
+          sitemap: null,
+          host: null
+        }
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/some-other-sitemap.xml`,
+        exclude: ["/services/*", `/blog/*`],
+        query: `
+        {
+          site {
+            siteMetadata {
+              siteUrl
+            }
+          }
+ 
+          allSitePage {
+            edges {
+              node {
+                path
+              }
+            }
+          }
+      }`
+      }
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
