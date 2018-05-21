fb8ccc28edd3d3321e3fa424bdbbfd37bf9b07ed
Philip Speake <philip_speake@yahoo.com>
2018-05-21 09:44:18 -0700

    added majestic meta tag

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -16,6 +16,7 @@ const TemplateWrapper = ({ children }) => (
         content="WJP7leNv9TY1mVO8CwZKT9WcKrbSrYIWCw2daBveq2M"
       />
       <meta name="p:domain_verify" content="26b19b0552a0b6d518f4e8de5693df1b"/>
+      <meta name="majestic-site-verification" content="MJ12_885a5606-3842-4a7f-b0d9-510064a1dd03">
       <meta name="msvalidate.01" content="4BC90264D5A19209C3797B0679F86CC6" />
       <meta
         name="description"
